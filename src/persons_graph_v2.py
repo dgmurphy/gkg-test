d1002d1a34772d54194ae8e29c6498084fed0527
Doug Murphy <murphy.dg@gmail.com>
2020-03-04 16:29:36 -0500

    persons_graph_v2.py

--- src/persons_graph_v2.py
+++ src/persons_graph_v2.py
@@ -48,27 +48,51 @@ def make_persons_pairs(persons_line):
     return pairs
 
 
+def names_to_ids(row):
+    print(row.name)
+    #print(str(row))
+    
+
+
 def main():
 
-    # create blank full df
-    df = pd.DataFrame(columns=GKG_COLUMN_NAMES)
+    # # create blank full df
+    # df = pd.DataFrame(columns=GKG_COLUMN_NAMES)
 
-    file_list = glob.glob(ARMY_GKG_DAILY_DIR + "Army_GKG_by_day*.csv")
-    #file_list = glob.glob(ARMY_GKG_DAILY_DIR + "Army_GKG_by_day_2020-02-16.csv")
+    # file_list = glob.glob(ARMY_GKG_DAILY_DIR + "Army_GKG_by_day*.csv")
+    # #file_list = glob.glob(ARMY_GKG_DAILY_DIR + "Army_GKG_by_day_2020-02-16.csv")
 
-    for f in file_list:
+    # for f in file_list:
         
-        logging.info("reading" + f)
+    #     logging.info("reading" + f)
 
-        # read the file into temp df
-        tdf = pd.read_csv(f, header=0, sep='\t', names=GKG_COLUMN_NAMES, 
-            index_col=False)
+    #     # read the file into temp df
+    #     tdf = pd.read_csv(f, header=0, sep='\t', names=GKG_COLUMN_NAMES, 
+    #         index_col=False)
 
-        # append temp df to full df
-        df = df.append(tdf, ignore_index=True)
+    #     # append temp df to full df
+    #     df = df.append(tdf, ignore_index=True)
 
 
-    logging.info("consolidated df shape: " + str(df.shape))
+    # logging.info("consolidated df shape: " + str(df.shape))
+
+
+    # -------------------------------- DEBUG ------------------------------
+    # persons_rows = []
+    # persons_rows.append("n1; n2; n3; n4")
+    # persons_rows.append("n1; n3; n5; n6")
+    gkg_data = [
+        ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 
+         'col10', 'col11', 'n1; n2; n3; n4', 'col13', 'col14', 'col15', 'col16', 'col17', 'col18', 
+         'col19', 'col20', 'col21', 'col22', 'col23', 'col24', 'col25', 'col26', 'col27'],
+        ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 
+         'col10', 'col11', 'n1; n3; n5; n6', 'col13', 'col14', 'col15', 'col16', 'col17', 'col18', 
+         'col19', 'col20', 'col21', 'col22', 'col23', 'col24', 'col25', 'col26', 'col27'],         
+    ]
+
+    # create blank full df
+    df = pd.DataFrame(gkg_data, columns=GKG_COLUMN_NAMES)
+
 
 
     # ------ create person nodes (TODO this can be made faster) --------
@@ -109,21 +133,7 @@ def main():
 
 
     # -------------------- Build edge list ------------------------------------
-        # DEBUG
-    # persons_rows = []
-    # persons_rows.append("n1; n2; n3; n4")
-    # persons_rows.append("n1; n3; n5; n6")
-    gkg_data = [
-        ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 
-         'col10', 'col11', 'n1; n2; n3; n4', 'col13', 'col14', 'col15', 'col16', 'col17', 'col18', 
-         'col19', 'col20', 'col21', 'col22', 'col23', 'col24', 'col25', 'col26', 'col27'],
-        ['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 
-         'col10', 'col11', 'n1; n3; n5; n6', 'col13', 'col14', 'col15', 'col16', 'col17', 'col18', 
-         'col19', 'col20', 'col21', 'col22', 'col23', 'col24', 'col25', 'col26', 'col27'],         
-    ]
 
-    # create blank full df
-    #df = pd.DataFrame(gkg_data, columns=GKG_COLUMN_NAMES)
 
     # make persons column into set
     df['V1PERSONS'] = df.apply(lambda row: make_persons_pairs(row.V1PERSONS), axis=1)
@@ -153,6 +163,10 @@ def main():
     edgefile = GRAPH_DIR + "PersonsEdgeListSorted.csv"
     logging.info("writing " + edgefile)
     edge_df.to_csv(edgefile, header=True, index=True, sep=",")
+
+
+    # Write the edge list using node ids instead of names
+    edge_df['ids'] = edge_df.apply(lambda row: names_to_ids(row), axis=1)
 
     # edgefile = GRAPH_DIR + "PersonsEdgeListSorted.csv"
     # logging.info("writing " + edgefile)
